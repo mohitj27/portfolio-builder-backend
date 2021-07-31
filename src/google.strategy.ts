b9c354e99ec9e488ea7a17ec8b1c3520913a968e
mohit <mjain8156@gmail.com>
2021-07-31 21:23:31 +0530

    url change

--- src/google.strategy.ts
+++ src/google.strategy.ts
@@ -12,7 +12,7 @@ export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
     super({
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_SECRET,
-      callbackURL: 'http://localhost:3000/auth/google/redirect',
+      callbackURL: 'http://65.2.29.181:3000/auth/google/redirect',
       scope: ['email', 'profile'],
     });
   }
